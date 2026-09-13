abac693285e74d3b43d2aec7b774b987e1d2da49
Артём <boltongenry-ops@users.noreply.github.com>
2026-09-13 21:10:00 +0300

    fix: sticky actions column in desktop table

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'p5letka-journal-v2-v3';
+var CACHE = 'p5letka-journal-v2-v4';
 var ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', function(e){
