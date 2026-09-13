75ded4fef8dc776cbf1abc8759bfd9fbe3154b81
Артём <boltongenry-ops@users.noreply.github.com>
2026-09-13 20:55:57 +0300

    style: calmer palette, type scale and states polish (CSS only)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'p5letka-journal-v2-v1';
+var CACHE = 'p5letka-journal-v2-v2';
 var ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', function(e){
